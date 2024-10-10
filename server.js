5d31033a35531b28775eb47350573ac027b5705d
Eliphus Godsave <godsaveeliphusx@gmail.com>
2024-10-10 13:54:16 -0700

    DATABASE WEEK 5

--- server.js
+++ server.js
@@ -1,70 +1,74 @@
-const express = require('express');
+// import some dependecies or packages
+ // express framework- it is used for handling http request 
+
+ const express =require('express');
+
+// create an intance of the framework 
 const app = express();
-const mysql = require('mysql2');
-const dotenv = require('dotenv');
+// dbms mysql
+ const mysql =require('mysql2');
+// cross origin resource sharing 
 const cors = require('cors');
+// environment variable 
+const dotenv = require('dotenv');
+
 
 app.use(express.json());
 app.use(cors());
 dotenv.config();
 
-// Database connection setup
-const db = mysql.createConnection({
+// connecting to the databse 
+const db= mysql.createConnection({
     host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    user:process.env.DB_USER,
+     // password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME
+})
+
+// check for the connection 
+db.connect((error) => {
+// if there is no connection= no wedding 
+ if(error) return console.log('error connecting to MYSQL');
+  // if yes there is connection = wedding go on 
+  console.log("connected to mysql as id: " , db.threadId);
+})
+
+// GET METHOD CODE GOES HERE
+// BRINGING THE VIEWS TO SERVER.JS GOES BELOW 
+
+app.set('view engine' , 'ejs')
+app.set('views', __dirname+'/views');
+
+// USING THE GET METHOD NOW 
+app.get('/data' , (req,res ) => {
+    // retrive the data from databse 
+    db.query('SELECT * FROM patients' , (err,results )=> {
+        if(err){
+            console.error(err)
+            res.status(500).send('error retrieving data ')
+        }else{
+            // if no error display the patients records to the browser 
+            res.render('data', {results:results});
+
+        }
+
+    })
 });
 
-// Check database connection
-db.connect(err => {
-    if (err) {
-        console.error('Error connecting to MySQL:', err);
-        return;
-    }
-    console.log('Connected to MySQL as id:', db.threadId);
-});
+// STOP GET METHOD CODE HERE 
 
-// Retrieve all patients
-app.get('/patients', (req, res) => {
-    const query = 'SELECT patient_id, first_name, last_name, date_of_birth FROM Patients';
-    db.query(query, (err, results) => {
-        if (err) return res.status(500).json({ error: err.message });
-        res.json(results);
-    });
-});
 
-// Retrieve all providers
-app.get('/providers', (req, res) => {
-    const query = 'SELECT first_name, last_name, provider_specialty FROM Doctors';
-    db.query(query, (err, results) => {
-        if (err) return res.status(500).json({ error: err.message });
-        res.json(results);
-    });
-});
 
-// Filter patients by first name
-app.get('/patients/first-name/:firstName', (req, res) => {
-    const firstName = req.params.firstName;
-    const query = 'SELECT * FROM Patients WHERE first_name = ?';
-    db.query(query, [firstName], (err, results) => {
-        if (err) return res.status(500).json({ error: err.message });
-        res.json(results);
-    });
-});
+// THE SERVER 
+app.listen(process.env.PORT, () => {
+    console.log(`server listening on port${process.env.PORT}`);
+    
+    // send  a message to the browser 
+     console.log('sending message to the browser....')
+     app.get('/', (req , res ) => {
+        res.send('YEY!!!! wedding can proceed , the server started succcessfully !!!!');
+     });
 
-// Retrieve providers by specialty
-app.get('/providers/specialty/:specialty', (req, res) => {
-    const specialty = req.params.specialty;
-    const query = 'SELECT * FROM Doctors WHERE provider_specialty = ?';
-    db.query(query, [specialty], (err, results) => {
-        if (err) return res.status(500).json({ error: err.message });
-        res.json(results);
-    });
-});
 
-// Listen to the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+
+     });
